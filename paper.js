073b4b324b888fa9e3930df8e879b38b45bc620f
AarinS <77874039+AarinS@users.noreply.github.com>
2021-03-21 19:33:46 +1300

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,13 @@
+class Paper{
+  constructor(x,y){
+      this.image = loadImage("images/paper.js");
+
+      var options={
+          isStatic:false,
+          restitution:0,
+          friction:1,
+          density:1.2
+
+      }
+  }  
+}
